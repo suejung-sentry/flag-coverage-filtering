d34b8c83c6ed0291733e14d18cc07a453cd3a39c
Suejung Shin <suejung.shin@sentry.io>
2025-04-01 15:29:37 -0700

    fix: Try to fix flags (#5)

--- unit-tests/test_add.py
+++ unit-tests/test_add.py
@@ -1,5 +1,8 @@
 from flag_coverage_filtering.lib import add
-
+from flag_coverage_filtering.lib2 import add2
 
 def test_add():
     assert add(1, 2) == 3
+
+def test_add_2():
+    assert add2(1, 2) == 3
